d222478dbfe87f6991d6d2fc7f4b074a4bf6d032
n1kol1vay <137178010+Nigolet@users.noreply.github.com>
2024-12-12 18:52:17 +0100

    My first 3d

---
+++ vite-react/src/App.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable react/no-unknown-property */
+import { Canvas, useFrame } from '@react-three/fiber';
+import { OrbitControls, Sparkles } from '@react-three/drei';
+import { useRef } from 'react';
+
+const RotatingCube = () => {
+  const meshRef = useRef();
+
+  useFrame(() => {
+    if(meshRef.current) {
+      meshRef.current.rotation.y += 0.01;
+      meshRef.current.rotation.x += 0.01;
+    }
+  })
+
+  return (
+    <mesh ref={meshRef}>
+      <cylinderGeometry args={[1, 1, 1]} />
+      <meshLambertMaterial color="#433789" emissive="#433789" />
+
+      <Sparkles count={100} scale={1} size={6} speed={0.002} noise={0.2} color="orange" />
+    </mesh>
+  )
+}
+
+const App = () => {
+  return (
+    <Canvas style={{height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+      <OrbitControls enableZoom enablePan enableRotate />
+
+      <directionalLight position={[0, 0, 5]} intensity={1} color={0x9CDBA6} />
+
+      <color attach="background" args={['#F0F0F0']} />
+
+      <RotatingCube />
+    </Canvas>
+  );
+}
+
+export default App;
